9e1b83a7fde645bdefe0323cde9cfda6e1ac2c81
Kwame Boakye-Yiadom <74148076+boakye135@users.noreply.github.com>
2021-03-25 02:27:20 -0100

    I did a concatenation on all dataframes into a single data

--- vaccine.py
+++ vaccine.py
@@ -20,8 +20,8 @@ selected_country = st.sidebar.selectbox('Country',['Afghanistan','Albania','Alge
 'Ireland','Israel','Italy','Jamaica','Japan','Jersey','Jordan','Kazakhstan','Kenya','Kuwait','Laos','Latvia','Lebanon','Liechtenstein','Lithuania',
 'Luxembourg','Macao','Malawi','Malaysia','Maldives','Malta','Mauritius','Mexico','Moldova','Monaco','Mongolia','Montenegro','Montserrat','Morocco',
 'Mozambique','Myanmar','Nepal','Netherlands','Nigeria','Norway','Oman','Pakistan','Palau','Panama','Paraguay','Peru','Philippines','Poland',
-'Portugal','Qatar','Romania','Russia','Rwanda','Senegal','Serbia','Seychelles','Singapore','Slovakia','Slovenia','Spain','Sudan','Suriname',
-'Sweden','Switzerland','Thailand','Tunisia','Turkey','Uganda','Ukraine','Uruguay','Uzbekistan','Venezuela','Vietnam','Yemen','Zambia','Zimbabwe'])
+'Portugal','Qatar','Romania','Russia','Rwanda','Senegal','Serbia','Seychelles','Singapore','Slovakia','Slovenia','Spain','Suriname',
+'Sweden','Switzerland','Thailand','Tunisia','Turkey','Uganda','Ukraine','Uruguay','Venezuela','Vietnam','Zimbabwe'])
 
 @st.cache
 def load_data(Country):
@@ -48,12 +48,38 @@ st.header('Display Daily Vaccinations by Country.')
 st.write('Data Dimension: ' + str(covid_free.shape[0]) + ' rows and ' + str(covid_free.shape[1]) + ' columns.')
 st.dataframe(covid_free)
 
+
+countries = ['Afghanistan','Albania','Algeria','Andorra','Angola','Anguilla','Argentina','Australia','Austria','Azerbaijan',
+'Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Bermuda','Bolivia','Brazil','Bulgaria','Cambodia','Canada',
+'Chile','China','Colombia','Croatia','Cyprus','Czechia','Denmark','Dominica','Ecuador','Egypt','England','Estonia','Finland','France','Germany',
+'Ghana','Gibraltar','Greece','Greenland','Grenada','Guernsey','Guatemala','Guinea','Guyana','Honduras','Hungary','Iceland','India','Indonesia','Iran',
+'Ireland','Israel','Italy','Jamaica','Japan','Jersey','Jordan','Kazakhstan','Kenya','Kuwait','Laos','Latvia','Lebanon','Liechtenstein','Lithuania',
+'Luxembourg','Macao','Malawi','Malaysia','Maldives','Malta','Mauritius','Mexico','Moldova','Monaco','Mongolia','Montenegro','Montserrat','Morocco',
+'Mozambique','Myanmar','Nepal','Netherlands','Nigeria','Norway','Oman','Pakistan','Palau','Panama','Paraguay','Peru','Philippines','Poland',
+'Portugal','Qatar','Romania','Russia','Rwanda','Senegal','Serbia','Seychelles','Singapore','Slovakia','Slovenia','Spain','Suriname',
+'Sweden','Switzerland','Thailand','Tunisia','Turkey','Uganda','Ukraine','Uruguay','Venezuela','Vietnam','Zimbabwe']
+list_of_df = []
+full_table = pd.DataFrame()
+for country in countries:
+    url = "https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/country_data/" +country + '.csv'
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    raw = df.drop(df[df.vaccine == 'vaccine'].index) # Deletes repeating headers in content
+    raw = raw.drop(['Unnamed: 0'],axis = 1)
+    vaccine_data = raw.fillna(0)
+    list_of_df.append(vaccine_data)
+
+full_df = pd.concat(list_of_df,ignore_index=True)
+globally =  st.sidebar.checkbox('Total people vaccinated globally')
+if globally:
+    st.write(full_df)
+
 # Download data of vaccinated people
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="vaccinations.csv">Download CSV File</a>'
     return href
 
 st.markdown(filedownload(covid_free), unsafe_allow_html=True)
