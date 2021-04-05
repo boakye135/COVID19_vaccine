0eafc7977a9bc75751ac2a1b3efb17d0821176a4
Kwame Boakye-Yiadom <74148076+boakye135@users.noreply.github.com>
2021-04-05 12:10:07 +0000

    Update vaccine.py

--- vaccine.py
+++ vaccine.py
@@ -6,8 +6,7 @@ import seaborn as sns
 import numpy as np
 import altair as alt
 import plotly.express as px
-
-
+from PIL import Image
 
 
 countries = ['Afghanistan','Albania','Algeria','Andorra','Angola','Anguilla','Antigua and Barbuda','Argentina','Australia','Austria','Azerbaijan',
@@ -23,59 +22,21 @@ countries = ['Afghanistan','Albania','Algeria','Andorra','Angola','Anguilla','An
 'Sweden','Switzerland','Thailand','Togo','Trinidad and Tobago','Turks and Caicos Islands','Tunisia','Turkey','Uganda','Ukraine','United Arab Emirates',
 'United Kingdom','United States','Uruguay','Venezuela','Vietnam','Wales','Zimbabwe']
 
-menu = ["Home","DataFrame Analysis","Data Visualization","DataStorage","About"]
-choice = st.sidebar.selectbox("MENU",menu)
+#Covid vaccination data by country DataFrame
+@st.cache
+def load_data(Country):
+	url = "https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/country_data/" + Country + '.csv'
+	html = pd.read_html(url, header = 0)
+	df = html[0]
+	raw = df.drop(df[df.vaccine == 'vaccine'].index) # Deletes repeating headers in content
+	raw = raw.drop(['Unnamed: 0'],axis = 1)
+	vaccine_data = raw.fillna(0)
+	return vaccine_data
 
-if choice == "Home":
-	st.title('COVID-19 daily vaccination records')
-	st.markdown("""
-	This app performs simple webscraping of Daily COVID-19 vaccinations!
-	* **App by:** Nana Kwame Boakye
-	""")
-	st.sidebar.subheader("Select Input Features")
-	selected_country = st.sidebar.selectbox('Country',countries)
-
-
-	#for country in selected_country:
-	#	country = '%20'.join(country.split())
-	def load_data(Country):
-		url = "https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/country_data/" + Country + '.csv'
-		html = pd.read_html(url, header = 0)
-		df = html[0]
-		raw = df.drop(df[df.vaccine == 'vaccine'].index) # Deletes repeating headers in content
-		raw = raw.drop(['Unnamed: 0'],axis = 1)
-		vaccine_data = raw.fillna(0)
-		return vaccine_data
-	vaccine_data = load_data(selected_country)
-
-	# Sidebar - Vaccine type
-	sorted_unique_vaccine = sorted(vaccine_data.vaccine.unique())
-	selected_vaccine = st.sidebar.multiselect('Vaccine Type', sorted_unique_vaccine, sorted_unique_vaccine)
-
-	sorted_unique_date = sorted(vaccine_data.date.unique())
-	selected_date = st.sidebar.multiselect('Vaccination Date',sorted_unique_date,sorted_unique_date)
-
-	# Filtering data
-	covid_free = vaccine_data[(vaccine_data.vaccine.isin(selected_vaccine)) & (vaccine_data.date.isin(selected_date))]
-	st.header('Display Daily Vaccinations by Country.')
-	st.write('Data Dimension: ' + str(covid_free.shape[0]) + ' rows and ' + str(covid_free.shape[1]) + ' columns.')
-	st.dataframe(covid_free)
-
-	# Download data of vaccinated people
-	# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-	def filedownload(df):
-		csv = df.to_csv(index=False)
-		b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-		href = f'<a href="data:file/csv;base64,{b64}" download="vaccinations.csv">Download CSV File</a>'
-		return href
-
-	st.markdown(filedownload(covid_free), unsafe_allow_html=True)
-
-
-
-elif choice == 'DataFrame Analysis':
-	url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
-	big_data = pd.read_csv(url,header = 0)
+#Global Covid dataframe generation
+@st.cache
+def this_data():
+	big_data = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv',header = 0)
 	raw = big_data.drop(big_data[big_data.location == 'Asia'].index)
 	raw = raw.drop(raw[raw.location == 'International'].index)
 	raw = raw.drop(raw[raw.location == 'Africa'].index)
@@ -85,63 +46,223 @@ elif choice == 'DataFrame Analysis':
 	raw = raw.drop(raw[raw.location == 'Europe'].index)
 	raw = raw.drop(raw[raw.location == 'World'].index)
 	big_df = raw.fillna(0)
-	sample_df = big_df[['iso_code','location','continent','date','total_cases','new_cases','total_deaths',
-	                    'people_vaccinated', 'people_fully_vaccinated', 'new_vaccinations','population','total_vaccinations',
-			            'people_vaccinated_per_hundred','people_fully_vaccinated_per_hundred','total_vaccinations_per_hundred']]
+	return big_df
+my_df = this_data()
+sample_df = my_df[['iso_code','location','continent','date','total_cases','new_cases','total_deaths',
+				'people_vaccinated', 'people_fully_vaccinated', 'new_vaccinations','population','total_vaccinations',
+				'people_vaccinated_per_hundred','people_fully_vaccinated_per_hundred','total_vaccinations_per_hundred']]
+
+   #Menu Sidebar
+menu = ["Home","Vaccination","Data Visualization","About"]
+choice = st.sidebar.selectbox("MENU",menu)
+
+if choice == "Home":
+	st.title('COVID-19 daily vaccination records')
+	st.markdown("""
+	This app performs simple webscraping of Daily COVID-19 vaccinations!
+	* **App by:** Nana Kwame Boakye
+	""")
+	image = Image.open('coro.jpeg')
+	st.image(image,use_column_width = True)
+	st.markdown('<style>body{background-color: lightblue;}</style>',unsafe_allow_html = True)
+
+
+elif choice == 'Vaccination':
 
    #Options menu for the Dataframe Analysis choice
-	options =['DataFrame','Piechart','Combine']
+	options =['Vaccination By Countries','Global Vaccination','Combine']
 	globally =  st.sidebar.selectbox('Total people vaccinated globally',options)
-	if globally == 'DataFrame':
-		st.write(sample_df)
-	elif globally == 'Combine':
-		c1,c2 = st.beta_columns(2)
-		cols = ['location', 'total_vaccinations', 'iso_code', 'total_vaccinations_per_hundred']
-		with c1:
-			sns.set_style('darkgrid')
-			vacc_amount = sample_df[cols].groupby('location').max().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
-			vacc_amount = vacc_amount.iloc[:10]
-			vacc_amount = vacc_amount.sort_values('total_vaccinations_per_hundred', ascending=False)
-			plt.figure(figsize=(12, 12))
-			sns.barplot(y = vacc_amount.index, x = vacc_amount.total_vaccinations_per_hundred,  palette = 'GnBu_d')
-			plt.ylabel('Countries')
-			plt.xlabel('Number of vaccinated people per hundred')
-		    #plt.xticks(rotation = 45)
-			st.set_option('deprecation.showPyplotGlobalUse', False)
-			st.subheader('DataFrame')
-			st.pyplot()
 
-		#chart 2
-		with c2:
-			vacc_amount = sample_df[cols].groupby('location').max().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
-			vacc_amount = vacc_amount.iloc[:10]
-			plt.figure(figsize=(12, 12))
-			sns.barplot(y = vacc_amount.index, x =vacc_amount.total_vaccinations, color = 'r')
-			plt.title('Total people vaccinated per country')
-			plt.yticks(rotation = 60)
-			plt.ylabel('Countries')
-			plt.xlabel('Number of vaccinated citizens (per 10 Million)')
-			st.set_option('deprecation.showPyplotGlobalUse', False)
-			st.subheader('Visualization')
-			st.pyplot()
+	if globally == 'Vaccination By Countries':
+		st.sidebar.subheader("Select Input Features")
+		selected_country = st.sidebar.selectbox('Country',countries)
+
+		vaccine_data = load_data(selected_country)
+
+		# Sidebar - Vaccine type
+		sorted_unique_vaccine = sorted(vaccine_data.vaccine.unique())
+		selected_vaccine = st.sidebar.multiselect('Vaccine Type', sorted_unique_vaccine, sorted_unique_vaccine)
+
+		sorted_unique_date = sorted(vaccine_data.date.unique())
+		selected_date = st.sidebar.multiselect('Vaccination Date',sorted_unique_date,sorted_unique_date)
+
+		# Filtering data
+		covid_free = vaccine_data[(vaccine_data.vaccine.isin(selected_vaccine)) & (vaccine_data.date.isin(selected_date))]
+		st.header('Display Daily Vaccinations by Country.')
+		st.write('Data Dimension: ' + str(covid_free.shape[0]) + ' rows and ' + str(covid_free.shape[1]) + ' columns.')
+		st.dataframe(covid_free)
+
+		# Download data of vaccinated people
+		# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+		def filedownload(df):
+			csv = df.to_csv(index=False)
+			b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+			href = f'<a href="data:file/csv;base64,{b64}" download="vaccinations.csv">Download CSV File</a>'
+			return href
+
+		st.markdown(filedownload(covid_free), unsafe_allow_html=True)
+
+		  #brief article on covid vaccinations
+		st.write(str(covid_free.loc[0]['location']) + ' started its COVID-19 vaccination on ' + str(covid_free.iloc[-1]['date']) +
+		' and a total number of ' + str(covid_free['people_vaccinated'].sum()) + ' people have being vaccinated using ' + str(covid_free['vaccine'].unique()))
+
+	elif globally == 'Global Vaccination':
+		st.header('Display Global Covid-19 Data.')
+		st.write('Data Dimension: ' + str(sample_df.shape[0]) + ' rows and ' + str(sample_df.shape[1]) + ' columns.')
+		st.write(sample_df)
+
+	elif globally == 'Combine':
+		st.write('see you')
+
 	else:
 		st.header('Piechart')
 
-
-
-
 elif choice == "Data Visualization":
-	st.header('Data Visualization')
+	group = ['Global Data Vizualization','Visualization By Continents','Visualization by Countries']
+	group_vis = st.sidebar.selectbox('Select',group)
+
+	if group_vis == 'Global Data Visualization':
+		st.header('Global Data Visualization')
+
+	elif group_vis == 'Visualization By Continents':
+		#st.header('Visualization By Continents')
+		vis_continent = st.sidebar.selectbox('Select Chart Type',('Bar Chart','Pie Chart','Line Chart'))
+		st.markdown("## **Continental Analysis**")
+
+		 #DRAWING A BAR CHART
+		if vis_continent == 'Bar Chart':
+			continent_chart_select = st.sidebar.radio('Chart',('Total People Vaccinated','Vaccinated Per 100', 'People Fully Vaccinated','People Fully Vaccinated Per 100'))
+			cols = ['people_fully_vaccinated_per_hundred', 'total_vaccinations', 'continent', 'total_vaccinations_per_hundred','people_fully_vaccinated']
+			if continent_chart_select == 'Vaccinated Per 100': # chart of Vaccinated people per 100
+				sns.set_style('whitegrid')
+				vacc_amount = sample_df[cols].groupby('continent').mean().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
+				vacc_amount = vacc_amount.iloc[:7]
+				vacc_amount = vacc_amount.sort_values('total_vaccinations_per_hundred', ascending=False)
+				plt.figure(figsize=(4, 3))
+				sns.barplot(y = vacc_amount.index, x = vacc_amount.total_vaccinations_per_hundred,  palette = 'Paired')
+				plt.ylabel('Continent')
+				plt.xlabel('Number of vaccinated people per hundred')
+				st.set_option('deprecation.showPyplotGlobalUse', False)
+				st.pyplot()
+
+			elif continent_chart_select == 'Total People Vaccinated': #chart of 'Total People Vaccinated
+				sns.set_style('darkgrid')
+				vacc_amount = sample_df[cols].groupby('continent').max().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
+				vacc_amount = vacc_amount.iloc[:7]
+				plt.figure(figsize=(4, 3))
+				sns.barplot(y = vacc_amount.index, x =vacc_amount.total_vaccinations, palette = 'Paired')
+				plt.ylabel('Continent')
+				plt.xlabel('Number of vaccinated citizens')
+				st.set_option('deprecation.showPyplotGlobalUse', False)
+				st.pyplot()
+
+			elif continent_chart_select == 'People Fully Vaccinated': # Chart of People Fully Vaccinated
+				sns.set_style('darkgrid')
+				vacc_amount = sample_df[cols].groupby('continent').sum().sort_values('people_fully_vaccinated', ascending=False).dropna(subset=['people_fully_vaccinated'])
+				vacc_amount = vacc_amount.iloc[:7]
+				plt.figure(figsize=(4, 3))
+				sns.barplot(y = vacc_amount.index, x =vacc_amount.people_fully_vaccinated, palette = 'Paired')
+				plt.ylabel('Continent')
+				plt.xlabel('Number of People Fully Vaccinated')
+				st.set_option('deprecation.showPyplotGlobalUse', False)
+				st.pyplot()
+
+			else: #Chart of People Fully Vaccinated Per 100
+				sns.set_style('darkgrid')
+				vacc_amount = sample_df[cols].groupby('continent').mean().sort_values('people_fully_vaccinated_per_hundred', ascending=False).dropna(subset=['people_fully_vaccinated_per_hundred'])
+				vacc_amount = vacc_amount.iloc[:7]
+				plt.figure(figsize=(4, 3))
+				sns.barplot(y = vacc_amount.index, x =vacc_amount.people_fully_vaccinated_per_hundred, palette = 'Paired')
+				plt.ylabel('Continent')
+				plt.xlabel('people_fully_vaccinated_per_hundred')
+				st.set_option('deprecation.showPyplotGlobalUse', False)
+				st.pyplot()
+
+
+
+
+			#Drawing of the pie Chart
+		elif vis_continent == 'Pie Chart':
+			continent_select = st.sidebar.selectbox('Select Continent',sample_df['continent'].unique())
+			selected_continent = sample_df[sample_df['continent'] == continent_select]
+			status_select = st.sidebar.radio('Status',('total_cases','total_deaths','people_vaccinated', 'people_fully_vaccinated',))
+			st.markdown("Please **NOTE** the **0** represents continents that were not defined but valuable in our analysis.")
+			if status_select == 'total_cases':
+				st.header('Total Comfrimed cases')
+				fig = px.pie(sample_df, values = sample_df['total_cases'],names = sample_df['continent'])
+				st.plotly_chart(fig)
+			elif status_select == 'people_vaccinated':
+				st.header('People Vaccinated')
+				fig = px.pie(sample_df, values = sample_df['people_vaccinated'],names = sample_df['continent'])
+				st.plotly_chart(fig)
+			elif status_select == 'total_deaths':
+				st.header('Total Deaths')
+				fig = px.pie(sample_df, values = sample_df['total_deaths'],names = sample_df['continent'])
+				st.plotly_chart(fig)
+			else:
+				st.header('People Fully Vaccinated')
+				fig = px.pie(sample_df, values = sample_df['people_fully_vaccinated'],names = sample_df['continent'])
+				st.plotly_chart(fig)
+
+		      #Drawing Line chart
+		elif vis_continent == 'Line Chart':
+			st.header('This is a line chart.')
+
+
+
+	else:
+		country_chart_select = st.sidebar.radio('Chart',('Total People Vaccinated','Vaccinated Per 100', 'People Fully Vaccinated','People Fully Vaccinated Per 100'))
+		st.header('Visualization by Countries')
+		cols = ['people_fully_vaccinated_per_hundred', 'total_vaccinations', 'location', 'total_vaccinations_per_hundred','people_fully_vaccinated']
+		if country_chart_select == 'Vaccinated Per 100':
+			sns.set_style('whitegrid')
+			vacc_amount = sample_df[cols].groupby('location').max().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
+			vacc_amount = vacc_amount.iloc[:10]
+			vacc_amount = vacc_amount.sort_values('total_vaccinations_per_hundred', ascending=False)
+			plt.figure(figsize=(5, 4))
+			sns.barplot(y = vacc_amount.index, x = vacc_amount.total_vaccinations_per_hundred,  palette = 'Paired')
+			plt.ylabel('Countries')
+			plt.xlabel('Number of vaccinated people per hundred')
+			st.set_option('deprecation.showPyplotGlobalUse', False)
+			st.subheader('Chart Showing Top 10 Countries.')
+			st.pyplot()
+
+		elif country_chart_select == 'Total People Vaccinated':
+			sns.set_style('darkgrid')
+			vacc_amount = sample_df[cols].groupby('location').max().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
+			vacc_amount = vacc_amount.iloc[:10]
+			plt.figure(figsize=(5, 4))
+			sns.barplot(y = vacc_amount.index, x =vacc_amount.total_vaccinations, palette = 'Paired')
+			plt.ylabel('Countries')
+			plt.xlabel('Number of vaccinated citizens')
+			st.set_option('deprecation.showPyplotGlobalUse', False)
+			st.subheader('Total people vaccinated per country')
+			st.pyplot()
+
+		elif country_chart_select == 'People Fully Vaccinated': # Chart of People Fully Vaccinated
+				sns.set_style('darkgrid')
+				vacc_amount = sample_df[cols].groupby('location').sum().sort_values('people_fully_vaccinated', ascending=False).dropna(subset=['people_fully_vaccinated'])
+				vacc_amount = vacc_amount.iloc[:10]
+				plt.figure(figsize=(4, 3))
+				sns.barplot(y = vacc_amount.index, x =vacc_amount.people_fully_vaccinated, palette = 'Paired')
+				plt.ylabel('Country')
+				plt.xlabel('Number of People Fully Vaccinated')
+				st.set_option('deprecation.showPyplotGlobalUse', False)
+				st.pyplot()
+
+		else: #Chart of People Fully Vaccinated Per 100
+				sns.set_style('darkgrid')
+				vacc_amount = sample_df[cols].groupby('location').mean().sort_values('people_fully_vaccinated_per_hundred', ascending=False).dropna(subset=['people_fully_vaccinated_per_hundred'])
+				vacc_amount = vacc_amount.iloc[:10]
+				plt.figure(figsize=(4, 3))
+				sns.barplot(y = vacc_amount.index, x =vacc_amount.people_fully_vaccinated_per_hundred, palette = 'Paired')
+				plt.ylabel('Country')
+				plt.xlabel('people_fully_vaccinated_per_hundred')
+				st.set_option('deprecation.showPyplotGlobalUse', False)
+				st.pyplot()
+
+
+
 
 else:
 	st.title("About")
-	first,last = st.beta_columns(2)
-	first.subheader('COVID-19: What to do if..')
-	first.graphviz_chart("""
-	digraph{
-	come into contact>>like
-	like->share
-	}
-	""")
-	last.write('I selectbox')
-	last.image('C:\\Users\\Samuel\\Desktop\\vid.jpg',caption = 'getting the vaccine',use_column_width = True)
